eaf44d542d2ff46430a8a327babd51e827f3fba5
yasser.akbbach <yasser.akbbach@bell.ca>
2023-02-06 22:06:08 +0100

    implement splash screen for light/dark modes

--- app/src/main/java/com/yasserakbbach/diaryapp/MainActivity.kt
+++ app/src/main/java/com/yasserakbbach/diaryapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.yasserakbbach.diaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yasserakbbach.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryAppTheme {
 
