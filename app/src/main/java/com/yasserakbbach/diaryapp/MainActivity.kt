75e8ae532595d752c4a418831ed79fab3b620099
yasser.akbbach <yasser.akbbach@bell.ca>
2023-02-07 19:33:00 +0100

    add authentication screen to the nav graph and handle start destination

--- app/src/main/java/com/yasserakbbach/diaryapp/MainActivity.kt
+++ app/src/main/java/com/yasserakbbach/diaryapp/MainActivity.kt
@@ -4,22 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.yasserakbbach.diaryapp.navigation.Screen
 import com.yasserakbbach.diaryapp.navigation.SetupNavigationGraph
 import com.yasserakbbach.diaryapp.ui.theme.DiaryAppTheme
+import com.yasserakbbach.diaryapp.util.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             DiaryAppTheme {
                 SetupNavigationGraph(
-                    startDestination = Screen.Authentication,
+                    startDestination = getStartDestination(),
                     navController = rememberNavController(),
+                    onDataLoaded = {}
                 )
             }
         }
     }
+
+    private fun getStartDestination(): Screen =
+        App.create(APP_ID).currentUser?.run {
+            if(loggedIn) Screen.Home
+            else Screen.Authentication
+        } ?: Screen.Authentication
 }
--- app/src/main/java/com/yasserakbbach/diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/yasserakbbach/diaryapp/navigation/NavGraph.kt
@@ -1,32 +1,88 @@
 package com.yasserakbbach.diaryapp.navigation
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.stevdzasan.messagebar.rememberMessageBarState
+import com.stevdzasan.onetap.rememberOneTapSignInState
 import com.yasserakbbach.diaryapp.navigation.Routes.WRITE_DIARY_ID_ARG
+import com.yasserakbbach.diaryapp.presentation.screens.auth.AuthenticationScreen
+import com.yasserakbbach.diaryapp.presentation.screens.auth.AuthenticationViewModel
 
 @Composable
 fun SetupNavigationGraph(
     startDestination: Screen,
     navController: NavHostController,
+    onDataLoaded: () -> Unit,
 ) {
     NavHost(
         navController = navController,
         startDestination = startDestination.route,
     ) {
-        authenticationRoute()
+        authenticationRoute(
+            navigateToHome = {
+                navController.popBackStack()
+                navController.navigate(Screen.Home.route)
+            },
+            onDataLoaded = onDataLoaded
+        )
         homeRoute()
         writeDiaryRoute()
     }
 }
 
-private fun NavGraphBuilder.authenticationRoute() {
-    composable(route = Screen.Authentication.route) {
+@OptIn(ExperimentalMaterial3Api::class)
+private fun NavGraphBuilder.authenticationRoute(
+    navigateToHome: () -> Unit,
+    onDataLoaded: () -> Unit
+) {
 
+    composable(route = Screen.Authentication.route) {
+        val viewModel: AuthenticationViewModel = viewModel()
+        val loadingState by viewModel.loadingState
+        val authenticated by viewModel.authenticated
+        val oneTapState = rememberOneTapSignInState()
+        val messageBarState = rememberMessageBarState()
+
+        AuthenticationScreen(
+            authenticated = authenticated,
+            loadingState = loadingState,
+            oneTapState = oneTapState,
+            messageBarState = messageBarState,
+            onButtonClicked = {
+                oneTapState.open()
+                viewModel.setLoading(true)
+            },
+            onSuccessfulFirebaseSignIn = { tokenId ->
+                viewModel.signInWithMongoAtlas(
+                    tokenId = tokenId,
+                    onSuccess = {
+                        messageBarState.addSuccess("Successfully Authenticated!")
+                        viewModel.setLoading(false)
+                    },
+                    onError = {
+                        messageBarState.addError(it)
+                        viewModel.setLoading(false)
+                    }
+                )
+            },
+            onFailedFirebaseSignIn = {
+                messageBarState.addError(it)
+                viewModel.setLoading(false)
+            },
+            onDialogDismissed = { message ->
+                messageBarState.addError(Exception(message))
+                viewModel.setLoading(false)
+            },
+            navigateToHome = navigateToHome
+        )
     }
 }
 
