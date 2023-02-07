c4df45fc3593a36e74271d585f0f97d74012e6b8
yasser.akbbach <yasser.akbbach@bell.ca>
2023-02-07 13:44:57 +0100

    add Google Cloud client ID

--- app/src/main/java/com/yasserakbbach/diaryapp/util/Constants.kt
+++ app/src/main/java/com/yasserakbbach/diaryapp/util/Constants.kt
@@ -2,4 +2,5 @@ package com.yasserakbbach.diaryapp.util
 
 object Constants {
     const val APP_ID = "mydiaryapp-cdjsh"
+    const val CLIENT_ID = "848456860860-fvd73ab3pjv6ge2d7ob7beffbub9elk7.apps.googleusercontent.com"
 }
